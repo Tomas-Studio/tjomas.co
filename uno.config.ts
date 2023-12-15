3d9584913ae74b5aac30337e5e92e19d12723698
Olakunle Olatunji <tjomas20@gmail.com>
2023-12-15 16:35:29 +0100

    feat: added unocss and configuration

---
+++ uno.config.ts
@@ -0,0 +1,39 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+} from 'unocss'
+import extractorMdc from '@unocss/extractor-mdc'
+
+export default defineConfig({
+  shortcuts: {},
+  theme: {},
+  presets: [
+    presetUno(),
+    presetIcons(),
+    presetAttributify(),
+    presetWebFonts({
+      provider: 'bunny',
+      fonts: {
+        sans: 'DM Sans',
+        mono: 'DM Mono',
+      },
+    }),
+    presetTypography(),
+  ],
+  extractors: [
+    extractorMdc(),
+  ],
+  content: {
+    filesystem: [
+      './content/**/*.md',
+    ],
+  },
+  transformers: [
+    transformerDirectives(),
+  ],
+})
