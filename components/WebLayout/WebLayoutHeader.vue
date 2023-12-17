51c1afd31abe5a538a99d7120341272a064577c7
Olakunle Olatunji <tjomas20@gmail.com>
2023-12-17 20:57:25 +0100

    feat: added styles for scroll bar

--- components/WebLayout/WebLayoutHeader.vue
+++ components/WebLayout/WebLayoutHeader.vue
@@ -1,5 +1,9 @@
+<script lang="ts" setup>
+const { y: scroll } = useWindowScroll()
+</script>
+
 <template>
-  <header nav-w z-2>
+  <header nav-w z-2 :class="scroll > 300 ? 'backdrop-blur' : ''">
     <WebLayoutHeaderContent />
   </header>
 </template>
