ee504e9ae1911bb1a8b01c535457907c9935b007
Olakunle Olatunji <tjomas20@gmail.com>
2023-12-17 19:57:38 +0100

    feat: added animation to profile link

--- components/ProfileLink.vue
+++ components/ProfileLink.vue
@@ -11,11 +11,18 @@ const route = useRoute()
 
 <template>
   <div relative inline-flex>
-    <NuxtLink :to="to" :aria-label="name" class="profile-link" :class="[route.fullPath.includes(to) ? 'text-brand' : '']">
+    <NuxtLink v-tooltip.bottom="name" :to="to" :aria-label="name" class="profile-link" :class="[route.fullPath.includes(to) ? 'text-brand' : '']">
       <div aria-hidden="true" :class="icon" sm:hidden w5 h5 z5 />
       <span lt-sm:hidden text-3.75 z5 v-html="name" />
       <span v-show="route.fullPath.includes(to)" class="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-brand/0 via-brand to-brand/0 dark:(via-brand-dark)" />
-      <span v-show="route.fullPath.includes(to)" class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h8 w8 bg-gray-1 dark:bg-white/5 rounded-full" />
+      <span v-if="route.fullPath.includes(to)" class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-1 dark:bg-white/5 rounded-full animate-[expand_1s_ease]" :class="[route.fullPath.includes(to) ? 'h8 w8' : '']" />
     </NuxtLink>
   </div>
 </template>
+
+<style>
+@keyframes expand {
+  0% { height: 0; width: 0; }
+  100%{ height: 2rem; width: 2rem; }
+}
+</style>
