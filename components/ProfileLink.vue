4d69c358bc95c5b00f0e039dc83bd4091d8a37fd
Olakunle Olatunji <tjomas20@gmail.com>
2023-12-18 06:09:21 +0100

    refactor: profileLink component

--- components/ProfileLink.vue
+++ components/ProfileLink.vue
@@ -6,20 +6,21 @@ type Props = {
   icon: string
   name: string
 }
-defineProps<Props>()
+const { to } = defineProps<Props>()
 
 const route = useRoute()
+const active = computed(() => route.fullPath.includes(to))
 const breakpoints = useBreakpoints(breakpointsTailwind)
 const largerThanSm = breakpoints.greater('sm')
 </script>
 
 <template>
   <div relative inline-flex>
-    <NuxtLink v-tooltip.bottom="{ content: name, disabled: largerThanSm }" :to="to" :aria-label="name" class="profile-link" :class="[route.fullPath.includes(to) ? 'text-brand' : '']">
+    <NuxtLink v-tooltip.bottom="{ content: name, disabled: largerThanSm }" :to="to" :aria-label="name" class="profile-link" :class="active ? 'text-brand' : ''">
       <div aria-hidden="true" :class="icon" sm:hidden w5 h5 z5 />
       <span lt-sm:hidden text-3.75 z5 v-html="name" />
-      <span v-show="route.fullPath.includes(to)" class="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-brand/0 via-brand to-brand/0 dark:(via-brand-dark)" />
-      <span v-if="route.fullPath.includes(to)" class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-1 dark:bg-white/5 rounded-full animate-[expand_1s_ease]" :class="[route.fullPath.includes(to) ? 'h8 w8' : '']" />
+      <span v-show="active" class="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-brand/0 via-brand to-brand/0 dark:(via-brand-dark)" />
+      <span v-if="active" class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-1 dark:bg-white/5 rounded-full animate-[expand_1s_ease]" :class="active ? 'h8 w8' : ''" />
     </NuxtLink>
   </div>
 </template>
