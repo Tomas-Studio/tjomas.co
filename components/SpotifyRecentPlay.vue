8fdcaf9fc213fee193152b7dbc567c229322c117
Olakunle Olatunji <tjomas20@gmail.com>
2023-12-27 13:07:47 +0100

    feat: added new footer and styled it

--- components/SpotifyRecentPlay.vue
+++ components/SpotifyRecentPlay.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="sm:w70% p2.5 rounded-full bg-brand-green fyc gap-x-4">
-    <div i-fa6-brands-spotify h10 w10 shrink-0 />
+  <div class="sm:w70% p3 rounded-full bg-brand-green fyc gap-x-4">
+    <div aria-hidden="true" i-fa6-brands-spotify h10 w10 shrink-0 />
     <div>
       <span block text-sm mb--1 text-gray-7>On repeat</span>
       <NuxtLink
         to="https://open.spotify.com/track/1BncfTJAWxrsxyT9culBrj?si=JtSiXGMiTImM3CFitaYd4g"
         target="_blank" focus-visible:underline outline-none
         aria-label="Play Experience by Ludovico Einaudi"
-        class="truncate ..."
+        class="truncate text-gray-2"
       >
         Experience • Ludovico Einaudi et al
       </NuxtLink>
--- components/WebLayout/WebLayoutHeaderContent.vue
+++ components/WebLayout/WebLayoutHeaderContent.vue
@@ -7,7 +7,7 @@
     <ProfileLink name="projects" to="/projects" icon="i-solar:folder-outline" />
     <ProfileLink name="experience" to="/experience" icon="i-simple-icons:readdotcv" />
     <ProfileLink name="About" to="/about" icon="i-simple-icons-aboutdotme" />
-    <ProfileIconLink name="Twitter" to="https://x.com/" icon="i-line-md:twitter-x" />
+    <ProfileIconLink name="Twitter" to="https://x.com/@TunjiOlakunle" icon="i-line-md:twitter-x" />
     <ProfileIconLink name="Github" to="https://github.com/Tomas-Studio" icon="i-line-md:github" />
     <ProfileIconLink name="RSS" to="https://github.com/Tomas-Studio" icon="i-streamline:rss-square" />
     <ColorSchemeToggle />
