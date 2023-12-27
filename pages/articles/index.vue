55cb0da7d058b609fd8a83357a1f913abb000b27
Olakunle Olatunji <tjomas20@gmail.com>
2023-12-27 21:38:07 +0100

    chore: created folders and move things around again

---
+++ pages/articles/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+useHead({ title: 'Articles' })
+</script>
+
+<template>
+  <div web-w class="font-acorn text-5xl">
+    Articles
+  </div>
+</template>
---
+++ pages/projects/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+useHead({ title: 'Projects' })
+</script>
+
+<template>
+  <div web-w text-5xl class="font-acorn">
+    Projects
+  </div>
+</template>
