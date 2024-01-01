c7628f4074bec80c8e29a0079ed28dc9aca3ad8d
Olakunle Olatunji <tjomas20@gmail.com>
2024-01-01 21:55:44 +0100

    feat: fetched track on server & refactored code

--- components/SpotifyRecentPlay.vue
+++ components/SpotifyRecentPlay.vue
@@ -1,34 +1,12 @@
 <script setup lang="ts">
 import { useMotions } from '@vueuse/motion'
 
-const player = ref<any>(null)
-// get tracks sections
-const { data: token } = await useFetch('/api/authorize')
-const ids = '6pWgRkpqVfxnj3WuIcJ7WP,6ZFbXIJkuI1dVNWvzJzown,3CLSHJv5aUROAN2vfOyCOh'
-const query = { ids }
-const { data } = await useFetch<any>('https://api.spotify.com/v1/tracks', {
-  headers: {
-    Authorization: `Bearer ${token.value}`,
-  },
-  params: query,
-  deep: false,
-})
-
-const selectedTrack = computed(() => {
-  return data.value.tracks![Math.floor(Math.random() * data.value.tracks!.length)]
-})
+// get a track to preview
+const { data: track } = await useFetch('/api/track')
 
 // audio player section
-const isPaused = ref<boolean>(false)
-function play() {
-  // console.log(player.value);
-  isPaused.value = player.value!.paused
-  isPaused.value ? player.value?.play() : player.value?.pause()
-}
-
-// watch(()=> selectedTrack, ()=> {
-//   player.value = player.value
-// })
+const audioTag = ref<HTMLAudioElement | null>(null)
+const { play, isPaused } = useAudioTrack(audioTag)
 
 // animate section
 const show = ref(false)
@@ -40,7 +18,7 @@ const enter = ref({ width: 180, opacity: 1, marginLeft: 4 })
 
 <template>
   <div
-    v-if="data && data.tracks.length > 0"
+    v-if="track"
     bg-brand-green fixed bottom-4 left-3
     wmax p1.5 rounded-full fyc
     transition-all duration-300
@@ -66,7 +44,7 @@ const enter = ref({ width: 180, opacity: 1, marginLeft: 4 })
         ]"
       />
     </button>
-    <audio :ref="(el) => { player = el }" aria-hidden="true" :title="selectedTrack.name" sr-only :src="selectedTrack.preview_url" />
+    <audio ref="audioTag" aria-hidden="true" sr-only :src="track.preview_url" />
     <Transition :css="false" @leave="(_: any, done: () => void) => motions.title.leave(done)">
       <div
         v-if="show"
@@ -78,12 +56,17 @@ const enter = ref({ width: 180, opacity: 1, marginLeft: 4 })
         relative
       >
         <p class="text-3.5 text-gray-2 fw500 leading-none truncate">
-          <a :aria-label="`Open ${selectedTrack.name} on spotify`" :href="selectedTrack.external_urls.spotify" focus-visible:underline target="_blank" title="open">
-            {{ selectedTrack.name }}
+          <a
+            :aria-label="`Open ${track.name} on spotify`"
+            :href="track.external_urls.spotify"
+            focus-visible:underline
+            target="_blank" title="open"
+          >
+            {{ track.name }}
           </a>
         </p>
         <p text="~ 3 gray-9" leading-none truncate>
-          {{ selectedTrack.artists.map((e: any) => e.name).join(', ') }}
+          {{ track.artists.map((e: any) => e.name).join(', ') }}
         </p>
       </div>
     </Transition>
--- composables/useAudioTrack.ts
+++ composables/useAudioTrack.ts
@@ -1,18 +1,18 @@
-// import type { Ref } from 'vue'
+import type { Ref } from 'vue'
 
-export const usePlayer = () => useState<HTMLAudioElement | null>('audio-el', () => null)
+// export const usePlayer = () => useState<HTMLAudioElement | null>('audio-el', () => null)
 
 /**
  * Control audio tag / player
- * @param audioEl audio element
+ * @param el audio element
  * @returns play, isPaused
  */
-export default function () {
-  const el = usePlayer()
+export default function (el: Ref<HTMLAudioElement | null>) {
   const isPaused = useState<boolean>('is-playing', () => false)
 
   function play() {
-    // console.log(el.value);
+    if (el.value === null)
+      return
 
     isPaused.value = el.value!.paused
     isPaused.value ? el.value?.play() : el.value?.pause()
