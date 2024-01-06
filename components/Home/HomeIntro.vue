6bb4a56a220e2ad52be046cd62ebf862cf8e06a7
Olakunle Olatunji <tjomas20@gmail.com>
2024-01-06 03:33:27 +0100

    chore: minor adjustment

--- components/Home/HomeIntro.vue
+++ components/Home/HomeIntro.vue
@@ -16,39 +16,6 @@
           My focus is building maintainable, accessible & user-focused interfaces.
         </p>
       </div>
-      <div>
-        <!-- to be decided what to have here -->
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id nemo ipsum assumenda optio perspiciatis repudiandae hic aspernatur quos quibusdam, cum modi tenetur quas architecto accusantium voluptas nesciunt, dolorum cupiditate dolorem?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum natus delectus ex quia, vero facere quibusdam excepturi vitae cumque non earum tenetur distinctio adipisci unde quos dolore exercitationem, aspernatur maxime!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum esse quia, illo iste, illum fuga quidem dicta, rerum possimus facilis ratione officiis. Quod sit cum placeat error eaque maiores id?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero atque debitis pariatur iusto inventore commodi id placeat delectus hic, fugit molestias excepturi quo, velit porro, harum exercitationem vero cumque. Nam!
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id nemo ipsum assumenda optio perspiciatis repudiandae hic aspernatur quos quibusdam, cum modi tenetur quas architecto accusantium voluptas nesciunt, dolorum cupiditate dolorem?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum natus delectus ex quia, vero facere quibusdam excepturi vitae cumque non earum tenetur distinctio adipisci unde quos dolore exercitationem, aspernatur maxime!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum esse quia, illo iste, illum fuga quidem dicta, rerum possimus facilis ratione officiis. Quod sit cum placeat error eaque maiores id?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero atque debitis pariatur iusto inventore commodi id placeat delectus hic, fugit molestias excepturi quo, velit porro, harum exercitationem vero cumque. Nam!
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id nemo ipsum assumenda optio perspiciatis repudiandae hic aspernatur quos quibusdam, cum modi tenetur quas architecto accusantium voluptas nesciunt, dolorum cupiditate dolorem?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum natus delectus ex quia, vero facere quibusdam excepturi vitae cumque non earum tenetur distinctio adipisci unde quos dolore exercitationem, aspernatur maxime!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum esse quia, illo iste, illum fuga quidem dicta, rerum possimus facilis ratione officiis. Quod sit cum placeat error eaque maiores id?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero atque debitis pariatur iusto inventore commodi id placeat delectus hic, fugit molestias excepturi quo, velit porro, harum exercitationem vero cumque. Nam!
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id nemo ipsum assumenda optio perspiciatis repudiandae hic aspernatur quos quibusdam, cum modi tenetur quas architecto accusantium voluptas nesciunt, dolorum cupiditate dolorem?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum natus delectus ex quia, vero facere quibusdam excepturi vitae cumque non earum tenetur distinctio adipisci unde quos dolore exercitationem, aspernatur maxime!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum esse quia, illo iste, illum fuga quidem dicta, rerum possimus facilis ratione officiis. Quod sit cum placeat error eaque maiores id?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero atque debitis pariatur iusto inventore commodi id placeat delectus hic, fugit molestias excepturi quo, velit porro, harum exercitationem vero cumque. Nam!
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id nemo ipsum assumenda optio perspiciatis repudiandae hic aspernatur quos quibusdam, cum modi tenetur quas architecto accusantium voluptas nesciunt, dolorum cupiditate dolorem?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum natus delectus ex quia, vero facere quibusdam excepturi vitae cumque non earum tenetur distinctio adipisci unde quos dolore exercitationem, aspernatur maxime!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum esse quia, illo iste, illum fuga quidem dicta, rerum possimus facilis ratione officiis. Quod sit cum placeat error eaque maiores id?
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero atque debitis pariatur iusto inventore commodi id placeat delectus hic, fugit molestias excepturi quo, velit porro, harum exercitationem vero cumque. Nam!
-        </p>
-      </div>
     </div>
   </div>
 </template>
