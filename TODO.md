21a095f2a5897919e0fe472b73ab642174f39225
olakunle.olatunji <tjomas20@gmail.com>
2024-02-18 12:37:39 +0100

    feat: handle case for external articles and focus state

--- TODO.md
+++ TODO.md
@@ -1,7 +1,10 @@
 ## My Todos
 
 - [x] Add Slide up animation to article page
-- [ ] Add Table of content to article page
-- [ ] Create article preview card
+- [x] Add Table of content to article page
+- [x] Create article preview card
 - [ ] Add Og Image to every page
-- [ ] 
+- [ ] Add About me
+- [ ] Add Custom Error Page
+- [ ]
+- [ ]
--- components/ArticleCard.vue
+++ components/ArticleCard.vue
@@ -9,14 +9,19 @@ defineProps<Props>()
 </script>
 
 <template>
-  <NuxtLink :to="article._path" :aria-label="`Open this acticle on ${article.title}`" class="block mb6 group">
+  <NuxtLink
+    v-bind="article.external ? { href: article.external, target: '_blank', rel: 'noopener noreferrer' } : { to: article._path }"
+    :aria-label="`Open this acticle on ${article.title}`"
+    class="block mb6 group focus-visible:outline-none"
+  >
     <div>
-      <h2 class="font-acorn text-(xl balance) group-hover:text-black dark:group-hover:text-white duration-300">
-        {{ article.title }}
+      <h2 class="font-acorn text-(xl balance) group-hover:text-brand-green group-focus-visible:text-brand-green duration-300">
+        {{ article.title }} <span v-if="article.external" inline-block relative mb--1 ml2 h5 i-line-md-external-link-rounded />
       </h2>
       <div flex flex-col-reverse lt-sm:gap-y-3 sm:flex-row justify-between>
         <div class="fyc text-(sm gray-5)">
-          <span mr1 fw600 text-3.75>{{ formatDate(new Date(article.datePublished), 'MMM DD, YYYY') }} </span> <span fw700>•</span> <span ml1>{{ article.readingTime.text }}</span>
+          <span mr1 fw600 text-3.75>{{ formatDate(new Date(article.datePublished), 'MMM DD, YYYY') }} </span>
+          <span fw700>•</span> <span ml1>{{ article.readingTime.text }}</span>
         </div>
         <div fyc gap-x-2.5>
           <div i-solar-tag-horizontal-line-duotone text-sm />
