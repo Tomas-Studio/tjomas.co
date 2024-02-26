b1a94f956b073e11753ad6b5117ba525ae5f30aa
olakunle.olatunji <tjomas20@gmail.com>
2024-02-26 14:29:00 +0100

    chore: style edits

--- pages/tags/[slug].vue
+++ pages/tags/[slug].vue
@@ -30,9 +30,9 @@ const articles = await queryContent('articles').where({ tags: { $contains: `${ta
     <NuxtLink
       v-tooltip.left="'More tags'"
       to="/tags"
-      class="absolute left--20 top-5 px2 py1 focusable lt-md:hidden op50 hover:op100 duration-300"
+      class="absolute left--20 top-5 px3 py1.5 focusable lt-md:hidden op20 hover:op70 duration-300"
     >
-      <div text-2xl i-material-symbols:keyboard-backspace-rounded />
+      <div text-2xl i-ion:return-up-back />
     </NuxtLink>
     <div>
       <h1 class="font-acorn text-4xl">
