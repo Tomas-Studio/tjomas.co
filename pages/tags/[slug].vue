2992fddc178c522e51ec4aa6f1aa15bf6a5b9257
olakunle.olatunji <tjomas20@gmail.com>
2024-02-26 11:41:24 +0100

    feat: added back link on tag page

--- pages/tags/[slug].vue
+++ pages/tags/[slug].vue
@@ -26,24 +26,29 @@ const articles = await queryContent('articles').where({ tags: { $contains: `${ta
 </script>
 
 <template>
-  <div web-w pt6>
-    <div mb8>
-      <div>
-        <h1 class="font-acorn text-4xl">
-          Tag -  <span capitalize>{{ tag.title }}</span>
-        </h1>
-        <p mt3 sm:text-lg>
-          {{ tag.description }}
-        </p>
-        <div h1px w20 bg-brand-green mt />
-      </div>
-      <div class="mt8">
-        <div v-if="articles.length > 0" class="slide-enter-content">
-          <h2 class="uppercase text-(sm brand-yellow) fw700 tracking-wide mb6">
-            Articles
-          </h2>
-          <ArticleCard v-for="item of articles" :key="item._path" :article="item" />
-        </div>
+  <div web-w pt6 relative>
+    <NuxtLink
+      v-tooltip.left="'More tags'"
+      to="/tags"
+      class="absolute left--20 top-5 px2 py1 focusable lt-md:hidden op50 hover:op100 duration-300"
+    >
+      <div text-2xl i-material-symbols:keyboard-backspace-rounded />
+    </NuxtLink>
+    <div>
+      <h1 class="font-acorn text-4xl">
+        Tag -  <span capitalize>{{ tag.title }}</span>
+      </h1>
+      <p mt3 sm:text-lg>
+        {{ tag.description }}
+      </p>
+      <div h1px w20 bg-brand-green mt />
+    </div>
+    <div class="my8">
+      <div v-if="articles.length > 0" class="slide-enter-content">
+        <h2 class="uppercase text-(sm brand-yellow) fw700 tracking-wide mb6">
+          Articles
+        </h2>
+        <ArticleCard v-for="item of articles" :key="item._path" :article="item" />
       </div>
     </div>
   </div>
